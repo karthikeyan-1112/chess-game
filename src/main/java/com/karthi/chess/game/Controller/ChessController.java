360a4caad85c37bb7277617a42c8b43f3a244ae9
karthikeyan-1112 <karthikeyan.m1887@gmail.com>
2025-07-27 12:01:18 +0530

    65 % of the code completed with a minimal issues

--- src/main/java/com/karthi/chess/game/Controller/ChessController.java
+++ src/main/java/com/karthi/chess/game/Controller/ChessController.java
@@ -13,155 +13,146 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.bhlangonijr.chesslib.Board;
+import com.github.bhlangonijr.chesslib.Piece;
+import com.github.bhlangonijr.chesslib.PieceType;
 import com.github.bhlangonijr.chesslib.Side;
 import com.github.bhlangonijr.chesslib.Square;
 import com.github.bhlangonijr.chesslib.move.Move;
+import com.karthi.chess.game.model.ChessAI;
 
 @RestController
 public class ChessController implements InitializingBean {
 
+    private static final String STARTING_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     private final Board board = new Board();
 
     @Override
-    public void afterPropertiesSet() throws Exception {
-        board.loadFromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    public void afterPropertiesSet() {
+        board.loadFromFen(STARTING_FEN);
     }
 
     @GetMapping("/board")
     @SuppressWarnings("unchecked")
     public ResponseEntity<Map<String, Object>[][]> getBoard() {
         Map<String, Object>[][] boardState = (Map<String, Object>[][]) new HashMap[8][8];
-        Map<String, Object> response = new HashMap<>();
-
-        try {
-            for (Square square : Square.values()) {
-                if (square.ordinal() >= 64) continue;
-
-                int row = 7 - square.getRank().ordinal();
-                int col = square.getFile().ordinal();
-
-                var piece = board.getPiece(square);
-                if (piece != com.github.bhlangonijr.chesslib.Piece.NONE) {
-                    boardState[row][col] = new HashMap<>();
-                    boardState[row][col].put("type", piece.getPieceType().toString());
-                    boardState[row][col].put("color", piece.getPieceSide().toString().toLowerCase());
-                } else {
-                    boardState[row][col] = null;
+        for (Square square : Square.values()) {
+            if (square.ordinal() >= 64) continue;
+            int row = 7 - square.getRank().ordinal();
+            int col = square.getFile().ordinal();
+            var piece = board.getPiece(square);
+            if (!piece.equals(Piece.NONE)) {
+                boardState[row][col] = new HashMap<>();
+                boardState[row][col].put("type", piece.getPieceType().toString());
+                boardState[row][col].put("color", piece.getPieceSide().toString().toLowerCase());
+                
+               if (piece.getPieceType() == PieceType.KING) 
+               {
+                boolean inCheck = board.isKingAttacked(); 
+                boardState[row][col].put("inCheck", inCheck);
                 }
+            } else {
+                boardState[row][col] = null;
             }
-
-            response.put("success", true);
-            response.put("board", boardState);
-            return ResponseEntity.ok(boardState);
-        } catch (Exception e) {
-            response.put("success", false);
-            response.put("message", "Error getting board state: " + e.getMessage());
-            return ResponseEntity.internalServerError().body(boardState);
         }
+        return ResponseEntity.ok(boardState);
     }
-    @GetMapping("/test-chesslib")
 
-public String testChesslib() {
-    try {
-        Board board = new Board();
-        board.loadFromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-        return "Chesslib is working. Side to move: " + board.getSideToMove();
-    } catch (Exception e) {
-        return "Error: " + e.getMessage();
-    }
-}
-
-
-   @PostMapping("/move")
-public ResponseEntity<Map<String, Object>> move(@RequestBody Map<String, Object> moveData) {
-    Map<String, Object> response = new HashMap<>();
-
-    try {
-        // validate inputs...
-        int fromRow = (int) moveData.get("fromRow");
-        int fromCol = (int) moveData.get("fromCol");
-        int toRow = (int) moveData.get("toRow");
-        int toCol = (int) moveData.get("toCol");
-        String color = moveData.get("color").toString().toLowerCase();
-        Side playerSide = color.equals("white") ? Side.WHITE : Side.BLACK;
-
-        if (board.getSideToMove() != playerSide) {
-            response.put("success", false);
-            response.put("message", "It's not " + color + "'s turn!");
-            return ResponseEntity.ok(response);
-        }
-
-        Square from = Square.valueOf(String.format("%s%d", (char)('a' + fromCol), 8 - fromRow).toUpperCase());
-        Square to = Square.valueOf(String.format("%s%d", (char)('a' + toCol), 8 - toRow).toUpperCase());
-        Move move = new Move(from, to);
-
-        if (board.isMoveLegal(move, true)) {
-            board.doMove(move);
-            response.put("success", true);
-            response.put("message", "Move successful");
-            response.put("move", move.toString());
-
-            if (board.isMated()) {
-                response.put("checkmate", true);
-                response.put("winner", color);
-            }
-
-            return ResponseEntity.ok(response);
-        } else {
-            response.put("success", false);
-            response.put("message", "Invalid move");
-            return ResponseEntity.ok(response);
-        }
-
-    } catch (Exception e) {
-        response.put("success", false);
-        response.put("message", "Error processing move: " + e.getMessage());
-        return ResponseEntity.internalServerError().body(response);
-    }
-}
-
-
-    @PostMapping("/reset")
-    public ResponseEntity<Map<String, Object>> resetGame() {
+    @PostMapping("/move")
+    public ResponseEntity<Map<String, Object>> move(@RequestBody Map<String, Object> moveData) {
         Map<String, Object> response = new HashMap<>();
-        
         try {
-            board.loadFromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-            response.put("success", true);
-            response.put("message", "Game reset to starting position");
-            return ResponseEntity.ok().body(response);
+            int fromRow = (int) moveData.get("fromRow");
+            int fromCol = (int) moveData.get("fromCol");
+            int toRow = (int) moveData.get("toRow");
+            int toCol = (int) moveData.get("toCol");
+            String color = moveData.get("color").toString().toLowerCase();
+            Side playerSide = color.equals("white") ? Side.WHITE : Side.BLACK;
+
+            if (board.getSideToMove() != playerSide) {
+                response.put("success", false);
+                response.put("message", "Not " + color + "'s turn!");
+                return ResponseEntity.ok(response);
+            }
+
+            Square from = Square.valueOf(String.format("%s%d", (char)('a' + fromCol), 8 - fromRow).toUpperCase());
+            Square to = Square.valueOf(String.format("%s%d", (char)('a' + toCol), 8 - toRow).toUpperCase());
+            Move move = new Move(from, to);
+
+            if (board.isMoveLegal(move, true)) {
+                board.doMove(move);
+                response.put("success", true);
+                response.put("move", move.toString());
+                response.put("gameStatus", getCurrentGameStatus());
+            } else {
+                response.put("success", false);
+                response.put("message", "Invalid move");
+            }
+            return ResponseEntity.ok(response);
         } catch (Exception e) {
             response.put("success", false);
-            response.put("message", "Error resetting game: " + e.getMessage());
+            response.put("message", "Error: " + e.getMessage());
             return ResponseEntity.internalServerError().body(response);
         }
     }
+
+    @PostMapping("/bot-move")
+    public ResponseEntity<Map<String, Object>> botMove(@RequestBody Map<String, String> request) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            String color = request.get("color").toLowerCase();
+            Side aiSide = color.equals("white") ? Side.WHITE : Side.BLACK;
+
+            if (board.getSideToMove() != aiSide) {
+                response.put("success", false);
+                response.put("message", "Not " + color + "'s turn!");
+                return ResponseEntity.ok(response);
+            }
+
+            Move bestMove = ChessAI.getGreedyMove(board, aiSide);
+
+            if (bestMove != null && board.isMoveLegal(bestMove, true)) {
+                board.doMove(bestMove);
+                response.put("success", true);
+                response.put("move", bestMove.toString());
+                response.put("gameStatus", getCurrentGameStatus());
+            } else {
+                response.put("success", false);
+                response.put("message", "No legal moves available.");
+            }
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            response.put("success", false);
+            response.put("message", "Error: " + e.getMessage());
+            return ResponseEntity.internalServerError().body(response);
+        }
+    }
+
     @PostMapping("/valid-moves")
     public ResponseEntity<Map<String, Object>> getValidMoves(@RequestBody Map<String, Integer> input) {
         Map<String, Object> response = new HashMap<>();
         try {
             int fromRow = input.get("row");
             int fromCol = input.get("col");
-    
             String fromNotation = String.format("%s%d", (char)('a' + fromCol), 8 - fromRow).toUpperCase();
             Square fromSquare = Square.valueOf(fromNotation);
-    
+
             List<Move> legalMoves = board.legalMoves();
             List<Map<String, Integer>> validMoves = new ArrayList<>();
-    
-            for (Move move : legalMoves) {
-                if (move.getFrom() == fromSquare) {
-                    Square to = move.getTo();
-                    int toRow = 7 - to.getRank().ordinal();
-                    int toCol = to.getFile().ordinal();
-    
-                    Map<String, Integer> moveMap = new HashMap<>();
-                    moveMap.put("row", toRow);
-                    moveMap.put("col", toCol);
-                    validMoves.add(moveMap);
+
+            if (legalMoves != null) {
+                for (Move move : legalMoves) {
+                    if (move.getFrom().equals(fromSquare)) {
+                        Square to = move.getTo();
+                        int toRow = 7 - to.getRank().ordinal();
+                        int toCol = to.getFile().ordinal();
+                        Map<String, Integer> moveMap = new HashMap<>();
+                        moveMap.put("row", toRow);
+                        moveMap.put("col", toCol);
+                        validMoves.add(moveMap);
+                    }
                 }
             }
-    
+
             response.put("success", true);
             response.put("validMoves", validMoves);
             return ResponseEntity.ok(response);
@@ -171,6 +162,26 @@ public ResponseEntity<Map<String, Object>> move(@RequestBody Map<String, Object>
             return ResponseEntity.badRequest().body(response);
         }
     }
-    
-    
+
+    @PostMapping("/reset")
+    public ResponseEntity<Map<String, Object>> resetGame() {
+        board.loadFromFen(STARTING_FEN);
+        Map<String, Object> response = new HashMap<>();
+        response.put("success", true);
+        response.put("message", "Game reset.");
+        response.put("gameStatus", "normal");
+        return ResponseEntity.ok(response);
+    }
+
+    private String getCurrentGameStatus() {
+        if (board.isMated()) {
+            return board.isKingAttacked() ? 
+                "checkmate-" + (board.getSideToMove() == Side.WHITE ? "black" : "white") : 
+                "stalemate";
+        }
+        if (board.isKingAttacked()) {
+            return "check-" + (board.getSideToMove() == Side.WHITE ? "white" : "black");
+        }
+        return "normal";
+    }
 }
--- src/main/java/com/karthi/chess/game/model/ChessAI.java
+++ src/main/java/com/karthi/chess/game/model/ChessAI.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 import com.github.bhlangonijr.chesslib.Board;
 import com.github.bhlangonijr.chesslib.Piece;
+import com.github.bhlangonijr.chesslib.PieceType;
 import com.github.bhlangonijr.chesslib.Side;
 import com.github.bhlangonijr.chesslib.move.Move;
 
@@ -12,22 +13,24 @@ public class ChessAI {
 
     public static Move getGreedyMove(Board board, Side side) {
         List<Move> legalMoves = board.legalMoves();
+        if (legalMoves == null || legalMoves.isEmpty()) {
+            return null;
+        }
+
         Move bestMove = null;
         int maxValue = Integer.MIN_VALUE;
 
         for (Move move : legalMoves) {
-            // Skip if not our turn
             if (board.getSideToMove() != side) continue;
 
-            Piece targetPiece = board.getPiece(move.getTo());
+            Piece target = board.getPiece(move.getTo());
             int value = 0;
-
-            if (targetPiece != null &&
-                targetPiece != Piece.NONE &&
-                targetPiece.getPieceSide() != side) {
-
-                value = getPieceValue(targetPiece);
+            if (target != null && target != Piece.NONE && target.getPieceSide() != side) {
+                value = getPieceValue(target.getPieceType());
             }
+
+            // Add small random factor to avoid always choosing the first best move
+            value += new Random().nextInt(5);
 
             if (value > maxValue) {
                 maxValue = value;
@@ -36,26 +39,20 @@ public class ChessAI {
         }
 
         if (bestMove == null && !legalMoves.isEmpty()) {
-            // If no capturing moves, return a random legal move
             return legalMoves.get(new Random().nextInt(legalMoves.size()));
         }
 
         return bestMove;
     }
 
-    private static int getPieceValue(Piece piece) {
-        // Defensive null check
-        if (piece == null || piece == Piece.NONE || piece.getPieceType() == null) {
-            return 0;
-        }
-
-        return switch (piece.getPieceType()) {
+    private static int getPieceValue(PieceType type) {
+        if (type == null) return 0;
+        return switch (type) {
             case PAWN -> 10;
-            case KNIGHT -> 30;
-            case BISHOP -> 30;
+            case KNIGHT, BISHOP -> 30;
             case ROOK -> 50;
             case QUEEN -> 90;
-            case KING -> 900;
+            case KING -> 1000;
             default -> 0;
         };
     }
